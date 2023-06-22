664d1f85a1cf5fa0582988db0335ca347aeb36c0
Eriquy Jhordan <eriquy@gmail.com>
2023-06-22 17:45:11 -0300

    Feat: apply font everywhere

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Manrope } from 'next/font/google'
+const manrope = Manrope({ subsets: ['latin'] })
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${manrope.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
