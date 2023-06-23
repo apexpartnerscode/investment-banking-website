3a82d8954d3709a512dba54b4843c67f4ab577d3
Eriquy Jhordan <eriquy@gmail.com>
2023-06-23 13:53:10 -0300

    feat: create header component

---
+++ src/components/header.tsx
@@ -0,0 +1,62 @@
+import Link from 'next/link'
+import styles from './styles.module.css'
+import Image from 'next/image'
+
+interface HeaderProps {
+  link1Name: string
+  link1Href: string
+  link2Name: string
+  link2Href: string
+  link3Name: string
+  link3Href: string
+  link4Name: string
+  link4Href: string
+  link5Name: string
+  link5Href: string
+  logoImg: any
+}
+
+export default function Header(props: HeaderProps) {
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <Image
+          src={props.logoImg}
+          alt="Logo"
+          width={80}
+          height={26}
+          className={styles.logo}
+        />
+      </Link>
+      <nav>
+        <ul className={styles.linksContainer}>
+          <li>
+            <Link className={styles.link} href={props.link1Href ?? ''}>
+              {props.link1Name}
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} href={props.link2Href ?? ''}>
+              {props.link2Name}
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} href={props.link3Href ?? ''}>
+              {props.link3Name}
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} href={props.link4Href ?? ''}>
+              {props.link4Name}
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} href={props.link5Href ?? ''}>
+              {props.link5Name}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
---
+++ src/pages/[[...page]].tsx
@@ -0,0 +1,100 @@
+import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
+import { useRouter } from 'next/router'
+import {
+  BuilderComponent,
+  builder,
+  useIsPreviewing,
+  Builder,
+} from '@builder.io/react'
+import DefaultErrorPage from 'next/error'
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+
+// put your Public API Key you copied from Builder.io here
+const BUILDER_API_KEY = '59df490fe78040cdbef83a436347ab86'
+builder.init(BUILDER_API_KEY)
+
+export async function getStaticProps({
+  params,
+}: GetStaticPropsContext<{ page: string[] }>) {
+  const page =
+    (await builder
+      .get('page', {
+        userAttributes: {
+          urlPath: '/' + (params?.page?.join('/') || ''),
+        },
+      })
+      .toPromise()) || null
+
+  return {
+    props: {
+      page,
+    },
+    revalidate: 5,
+  }
+}
+
+export async function getStaticPaths() {
+  const pages = await builder.getAll('page', {
+    options: { noTargeting: true },
+  })
+
+  return {
+    paths: pages.map((page) => `${page.data?.url}`),
+    fallback: true,
+  }
+}
+
+Builder.registerComponent(
+  dynamic(() => import('../components/header')),
+  {
+    name: 'Header',
+    inputs: [
+      { name: 'link1Name', type: 'text' },
+      { name: 'link1Href', type: 'text' },
+      { name: 'link2Name', type: 'text' },
+      { name: 'link2Href', type: 'text' },
+      { name: 'link3Name', type: 'text' },
+      { name: 'link3Href', type: 'text' },
+      { name: 'link4Name', type: 'text' },
+      { name: 'link4Href', type: 'text' },
+      { name: 'link5Name', type: 'text' },
+      { name: 'link5Href', type: 'text' },
+      { name: 'logoImg', type: 'file' },
+    ],
+    image:
+      'https://tabler-icons.io/static/tabler-icons/icons-png/3d-cube-sphere-off.png',
+  },
+)
+
+export default function Page({
+  page,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <h1>Loading...</h1>
+  }
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const isPreviewing = useIsPreviewing()
+  if (!page && !isPreviewing) {
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex" />
+          <meta name="title"></meta>
+        </Head>
+        <DefaultErrorPage statusCode={404} />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <BuilderComponent model="page" content={page} />
+    </>
+  )
+}
