ed13ae591e5115aebdea2e833805f4f3f163947d
Eriquy Jhordan <eriquy@gmail.com>
2023-06-23 14:12:52 -0300

    fix: eslint problems

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
+/* eslint-disable react/no-unknown-property */
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { Manrope } from 'next/font/google'
 const manrope = Manrope({ subsets: ['latin'] })
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
