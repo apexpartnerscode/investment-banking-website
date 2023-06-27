7919ee1ef5d196ed89543d75d88645f0e401887b
Eriquy Jhordan <eriquy@gmail.com>
2023-06-27 15:20:40 -0300

    feat: add favicon, title and description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { Manrope } from 'next/font/google'
+import Head from 'next/head'
 const manrope = Manrope({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -13,6 +14,13 @@ export default function App({ Component, pageProps }: AppProps) {
           font-family: ${manrope.style.fontFamily};
         }
       `}</style>
+      <Head>
+        <title>Investment Banking | Apex Partners</title>
+        <meta
+          name="description"
+          content="Somos uma casa de investimentos completa. Oferecemos soluções financeiras personalizadas para pessoas e organizações de espírito empreendedor."
+        ></meta>
+      </Head>
       <Component {...pageProps} />
     </>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="data:image/svg+xml,%3Csvg xmlns=&#39;http://www.w3.org/2000/svg&#39; viewBox=&#39;0 0 32 32&#39;%3E%3Cpath fill=&#39;%230078d2&#39; d=&#39;M2.29 0h27.43v32H2.29z&#39;/%3E%3Cpath fill=&#39;%23fff&#39; d=&#39;M16 8l-9.14 8L16 24l9.14-8L16 8z&#39;/%3E%3C/svg%3E"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
